342c5413df15e66c7f3149fd587ed834f5c33530
Huyhoangskt0028 <huyhoangskt0028@gmail.com>
2023-03-02 12:58:43 +0700

    Completed

---
+++ WebEnterprise/Data/Migrations/20230302055538_1.9.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebEnterprise.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _19 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
